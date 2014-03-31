d108d517912d6bca9f6d4b5523a1365cd9e95a08
Tom Jakubowski <tom@crystae.net>
2014-03-30 21:13:30 -0700

    and now for something completely different!
    shades of collections::dlist

--- src/contamers/list.rs
+++ src/contamers/list.rs
@@ -1,24 +1,61 @@
-#[deriving(Show, Eq)]
-pub enum List<A> {
-    Cons(A, ~List<A>),
-    Nil
+#[deriving(Show)]
+pub struct List<A> {
+    priv head: Link<A>,
+    priv length: uint
+}
+
+#[deriving(Show)]
+struct Node<A> {
+    next: Link<A>,
+    value: A
+}
+
+type Link<A> = Option<~Node<A>>;
+
+impl<A> List<A> {
+    pub fn new() -> List<A> {
+        List { head: None, length: 0 }
+    }
 }
 
 impl<A> List<A> {
-    pub fn new() -> List<A> { Nil }
-}
+    fn push_node(&mut self, mut new_head: ~Node<A>) {
+        use std::mem::swap;
 
-impl<A> List<A> {
-    pub fn cons(x: A, xs: List<A>) -> List<A> {
-        Cons(x, ~xs)
+        match self.head {
+            None => {
+                self.head = Some(new_head);
+            },
+            Some(ref mut head) => {
+                swap(head, &mut new_head);
+                head.next = Some(new_head);
+            }
+        };
+        self.length += 1;
+    }
+
+    fn pop_node(&mut self) -> Option<~Node<A>> {
+        self.head.take().map(|mut front_node| {
+            self.head = front_node.next.take();
+            self.length -= 1;
+            front_node
+        })
+    }
+
+    pub fn push(&mut self, val: A) {
+        self.push_node(~Node { value: val, next: None });
+    }
+
+    pub fn pop(&mut self) -> Option<A> {
+        self.pop_node().map(|n| n.value)
     }
 }
 
 impl<A> FromIterator<A> for List<A> {
     fn from_iterator<T: Iterator<A>>(mut iterator: T) -> List<A> {
         let mut list: List<A> = List::new();
-        for element in iterator {
-            list = List::cons(element, list);
+        for el in iterator {
+            list.push(el);
         }
         list
     }
@@ -26,27 +63,47 @@ impl<A> FromIterator<A> for List<A> {
 
 impl<A> Container for List<A> {
     fn len(&self) -> uint {
-        match *self {
-            Nil => { 0 },
-            Cons(_, ref xs) => { 1 + xs.len() }
-        }
+        self.length
     }
 }
 
 #[cfg(test)]
 mod test {
-    use super::{List, Cons, Nil};
+    use super::List;
 
     #[test]
-    fn test_cons_empty_list() {
-        let x = Nil;
-        assert_eq!(List::cons(1, x), Cons(1, ~Nil));
+    fn test_pop_empty_list() {
+        let mut list: List<()> = List::new();
+        assert_eq!(list.pop(), None);
+    }
+
+    #[test]
+    fn test_push_pop_empty_list() {
+        let mut list = List::new();
+        list.push(1);
+        assert_eq!(list.pop(), Some(1));
+    }
+
+    #[test]
+    fn test_list_len() {
+        let mut list = List::new();
+        list.push(1);
+        assert_eq!(list.len(), 1);
+        list.pop();
+        assert_eq!(list.len(), 0);
+        list.pop();
+        assert_eq!(list.len(), 0);
     }
 
     #[test]
     fn test_from_iterator() {
-        let a = ~[1, 2, 3];
-        let b: List<int> = a.iter().map(|&x| x).collect();
-        assert_eq!(b, Cons(3, ~Cons(2, ~Cons(1, ~Nil))));
+        let vec = [1, 2, 3, 4];
+
+        let mut list: List<&int> = vec.iter().collect();
+        assert_eq!(list.len(), 4);
+
+        for i in vec.iter().rev() {
+            assert_eq!(list.pop(), Some(i));
+        }
     }
 }
